7abcdf80615cf9ca578b6499146346f048d0c6e8
Chris C <chris.cumming@saturdaymp.com>
2023-08-10 17:09:53 -0600

    Re-add the example code for the checkbox to test on Mac.

--- Source/ExampleClient/ViewController.cs
+++ Source/ExampleClient/ViewController.cs
@@ -10,22 +10,55 @@ namespace ExampleClient
         {
         }
 
-        public override void ViewDidLoad ()
+        public override void ViewDidLoad()
         {
-            base.ViewDidLoad ();
-            // Perform any additional setup after loading the view, typically from a nib.
+            base.ViewDidLoad();
+
+            Title = "Binding Example";
+
+
+            // Create a label for the CheckBox.
+            var bemCheckBoxLabel = new UILabel()
+            {
+                Text = "BEMCheckBox:",
+                Frame = new CoreGraphics.CGRect(10, 40, 125, 25)
+            };
+
 
             // Create the actual checkbox.  Remember to pass in the size you want as a CoreGraphics.
             var checkbox = new BEMCheckBox(new CoreGraphics.CGRect(140, 40, 25, 25));
 
+            // Wire up the events.  You don't have to do this if you don't care
+            // about the events.  An easy way to think of the events is:
+            //
+            // DidTapCheckBox: Before checkbox checked.
+            // AnimationDidStopForCheckBox: After checkbox checked.
+            //
+            // See BEMCheckBox for more details:
+            //
+            // https://github.com/Boris-Em/BEMCheckBox
+            checkbox.DidTap += DidTapEvent;
+            checkbox.AnimationDidStop += AnimationDidStopEvent;
+
+
             // Add the controls to the view.
+            View.AddSubview(bemCheckBoxLabel);
             View.AddSubview(checkbox);
         }
 
-        public override void DidReceiveMemoryWarning ()
+        // Fired before the checkbox animation completes but after the internal
+        // checkbox settings are updated with the new check/unchecked status (i.e.
+        // On property is updated).
+        private void DidTapEvent(object sender, EventArgs eventArgs)
         {
-            base.DidReceiveMemoryWarning ();
-            // Release any cached data, images, etc that aren't in use.
+            Console.WriteLine("In BeforeCheckBoxClickedEvent which is DidTapCheckBox in BEMCheckBox.");
+        }
+
+        // Fired after the checkbox animation completes.  If there are no animations then
+        // it won't be triggered.
+        private void AnimationDidStopEvent(object sender, EventArgs eventArgs)
+        {
+            Console.WriteLine("In AfterCheckBoxClickedEvent which is AnimationDidStopForCheckBox in BEMCheckBox.");
         }
     }
 }
